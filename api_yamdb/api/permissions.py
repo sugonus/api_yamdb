c9a868e6c451c3992c1249778a30d841eb5b704b
Андрей <temp60@mail.ru>
2022-12-15 17:12:01 +0300

    Mixin added

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+
+class IsAdminOrReadOnly(BasePermission):
+    """
+    Просмотр доступен всем пользователям.
+    """
+    def has_permission(self, request, view):
+        return (
+            request.method in SAFE_METHODS
+            or (request.user.is_authenticated
+                and request.user.is_admin)
+        )
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,19 +3,22 @@ from rest_framework import viewsets
 
 from reviews.models import Title, Category, Genre
 from .serializers import TitleSerializer, CategorySerializer, GenreSerializer
+from .permissions import IsAdminOrReadOnly
+from .mixins import MixinSet
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
+    permission_classes = (IsAdminOrReadOnly,)
     filter_backends = (DjangoFilterBackend,)
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryViewSet(MixinSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
-class GenreViewSet(viewsets.ModelViewSet):
+class GenreViewSet(MixinSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
