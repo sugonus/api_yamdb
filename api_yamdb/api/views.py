7fc79a33c72e92ef9be658f014574a5e81e730d5
Сергей Ендовицкий <lightfire.89@mail.ru>
2022-12-17 16:49:49 +0300

    66 passed

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status, viewsets, permissions, filters
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.db.models import Avg
 from rest_framework.decorators import action
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.shortcuts import get_object_or_404
@@ -96,7 +97,9 @@ class UserViewSet(viewsets.ModelViewSet):
 
 
 class TitleViewSet(viewsets.ModelViewSet):
-    queryset = Title.objects.all()
+    queryset = Title.objects.all().annotate(
+        Avg("reviews__score")
+    ).order_by("name")
     serializer_class = TitleSerializer
     permission_classes = (IsAdminOrReadOnly,)
     filter_backends = (DjangoFilterBackend,)
@@ -110,11 +113,16 @@ class TitleViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(MixinSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ("name",)
+    lookup_field = "slug"
 
 
 class GenreViewSet(MixinSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
