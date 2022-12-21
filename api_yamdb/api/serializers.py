1a059d342e1b4ab98bb0238d6b284e8a3aa3cb70
Сергей Ендовицкий <lightfire.89@mail.ru>
2022-12-21 21:15:27 +0300

    сериалайзер review

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -18,11 +18,10 @@ class ReviewSerializer(serializers.ModelSerializer):
     )
 
     def validate(self, data):
-        request = self.context['request']
-        author = request.user
-        title_id = self.context['view'].kwargs.get('title_id')
-        title = get_object_or_404(Title, pk=title_id)
-        if request.method == 'POST':
+        if self.context['request'].method == 'POST':   #перемещено и немного видоизменено
+            author = self.context['request'].user
+            title_id = self.context['view'].kwargs.get('title_id')
+            title = get_object_or_404(Title, pk=title_id)
             if Review.objects.filter(title=title, author=author).exists():
                 raise ValidationError('Вы не можете добавить более'
                                       'одного отзыва на произведение')
